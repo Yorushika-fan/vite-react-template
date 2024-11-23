bd9b50107a83eb2ce88934116dbf5ece0d0a5b1c
Yorushika <chenlaoshi0226@gmail.com>
2024-11-23 09:00:31 +0800

    feat(all): 配置component自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport(),
+    Components({
+      dirs: ['src/components', 'src/otherComponents'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
+  },
 });
